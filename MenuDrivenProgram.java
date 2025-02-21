35762d2f6d9bac8df0ca6e3c0c0a64cfb4871cf1
Angela Tenorio <tenorioangela.bsit@plmun.edu.ph>
2025-02-21 23:39:44 +0800

    Submitting OBJPROG Week 05 - Laboratory # 20

--- MenuDrivenProgram.java
+++ MenuDrivenProgram.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MenuDrivenProgram{
+public class MenuDrivenProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
